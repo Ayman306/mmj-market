4a7282677df36712ba3434b1de8bf071c49db58d
Ayman <aymanuddin1111.au@gmail.com>
2024-08-07 20:22:41 +0530

    optimised

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from '../../shared/components/slider/slider.component';
 import { ServicecardComponent } from '../../shared/components/servicecard/servicecard.component';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { EnquiryFormComponent } from '../../shared/components/enquiry-form/enquiry-form.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
@@ -13,100 +13,118 @@ import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,SliderComponent,ServicecardComponent,EnquiryFormComponent,FooterComponent,NgOptimizedImage],
-  providers:[ApiService],
+  imports: [
+    CommonModule,
+    SliderComponent,
+    ServicecardComponent,
+    EnquiryFormComponent,
+    FooterComponent,
+    NgOptimizedImage,
+  ],
+  providers: [ApiService],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit{
-
- constructor(private route:Router,private apiService:ApiService,private spinner: NgxSpinnerService){}
+export class HomeComponent implements OnInit {
+  constructor(
+    private route: Router,
+    private apiService: ApiService,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit(): void {
-    this.getAllCategory()
+    this.getAllCategory();
     // this.toaster.success("Welcome!")
   }
 
-categories:any
+  categories: any;
 
-
-  getAllCategory(){
-    this.spinner.show()
-    this.apiService.getAllCategory().subscribe(
+  getAllCategory() {
+    this.spinner.show();
+    this.apiService.getCategory().subscribe(
       (res) => {
         this.categories = res;
-        this.spinner.hide()
+        this.spinner.hide();
       },
       (err) => {
         console.log(err);
-        this.spinner.hide()
+        this.spinner.hide();
       }
     );
   }
 
-banner=[
-{
-    username:'Fashionista',
-    location:'Surathkal-Mangalore',
-        userImage:'../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
-    image:'../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
-    desc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
-    button:'Know More'
-  },
-  {
-    username:'Fashionista',
-    location:'Surathkal-Mangalore',
-    userImage:'../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
-    image:'../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
-    desc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
-    button:'Know More'
-  },
-  {
-    username:'Fashionista',
-    location:'Surathkal-Mangalore',
-    image:'../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
-        userImage:'../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
-    desc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
-    button:'Know More'
-  },
-  {
-    username:'Fashionista',
-    location:'Surathkal-Mangalore',
-    image:'../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
-        userImage:'../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
-    desc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
-    button:'Know More'
-  },
-  {
-    username:'Fashionista',
-    location:'Surathkal-Mangalore',
-    image:'../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
-        userImage:'../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
-    desc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
-    button:'Know More'
-  },
-  {
-    username:'Fashionista',
-    location:'Surathkal-Mangalore',
-    image:'../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
-        userImage:'../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
-    desc:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
-    button:'Know More'
-  },
-]
-carouselImages = [
-  { src: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg', alt: 'Slide 1' },
-  { src: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg', alt: 'Slide 2' },
-  { src: '../../../assets/images/redcharlie-mugDbuNnbd0-unsplash.jpg', alt: 'Slide 3' }
-];
+  banner = [
+    {
+      username: 'Fashionista',
+      location: 'Surathkal-Mangalore',
+      userImage: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      image: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
+      button: 'Know More',
+    },
+    {
+      username: 'Fashionista',
+      location: 'Surathkal-Mangalore',
+      userImage: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      image: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
+      button: 'Know More',
+    },
+    {
+      username: 'Fashionista',
+      location: 'Surathkal-Mangalore',
+      image: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      userImage: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
+      button: 'Know More',
+    },
+    {
+      username: 'Fashionista',
+      location: 'Surathkal-Mangalore',
+      image: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      userImage: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
+      button: 'Know More',
+    },
+    {
+      username: 'Fashionista',
+      location: 'Surathkal-Mangalore',
+      image: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      userImage: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
+      button: 'Know More',
+    },
+    {
+      username: 'Fashionista',
+      location: 'Surathkal-Mangalore',
+      image: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      userImage: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi optio eveniet debitis quibusdam suscipit provident ut.  ',
+      button: 'Know More',
+    },
+  ];
+  carouselImages = [
+    {
+      src: '../../../assets/images/force-majeure-00tlC0Clfrs-unsplash.jpg',
+      alt: 'Slide 1',
+    },
+    {
+      src: '../../../assets/images/martin-pechy-Rp2fQv6AM44-unsplash.jpg',
+      alt: 'Slide 2',
+    },
+    {
+      src: '../../../assets/images/redcharlie-mugDbuNnbd0-unsplash.jpg',
+      alt: 'Slide 3',
+    },
+  ];
 
-navigateTo(type:string,name:any){
-  if(type=='category'){
-    this.route.navigate(['/all-service'],{queryParams: { service: name }})
-  }else{
-    this.route.navigate(['/service'],{queryParams: { service: name }})
+  navigateTo(type: any) {
+    if (type?.key == 'job') {
+      this.route.navigate(['/jobs']);
+    } else {
+      this.route.navigate(['/all-service'], {
+        queryParams: { service: type?.id },
+      });
+    }
   }
-}
-
-
 }
