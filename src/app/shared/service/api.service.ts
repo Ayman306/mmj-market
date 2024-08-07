2803b2d2823242391473ac9c90252f6471aa591f
Ayman <aymanuddin1111.au@gmail.com>
2024-08-07 19:22:44 +0530

    Update: services

--- src/app/shared/service/api.service.ts
+++ src/app/shared/service/api.service.ts
@@ -3,18 +3,17 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
+  apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
-
-  getAllCategory(){
-    return this.http.get<any>(environment.apiUrl + '/category')
+  getCategory(id?: any) {
+    return this.http.post<any>(`${this.apiUrl}/category`, id);
   }
 
-  getAllJobs(id:any=''){
-    return this.http.post<any>(environment.apiUrl + `/job`,id)
+  getAllJobs(id?: any) {
+    return this.http.post<any>(`${this.apiUrl}/job`, id);
   }
-
 }
--- src/app/shared/service/shared.service.ts
+++ src/app/shared/service/shared.service.ts
@@ -1,33 +1,9 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-
   constructor() { }
 
-  categoryType=[
-    {
-      name:"Restraunt",
-      id:1
-    },
-    {
-      name:"Hotel",
-      id:2
-    },
-    {
-      name:"Gym",
-      id:3
-    },
-    {
-      name:"Events",
-      id:4
-    },
-    {
-      name:"Jobs",
-      id:5
-    }
-
-  ]
 }
