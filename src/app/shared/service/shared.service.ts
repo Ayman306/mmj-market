d5d3c098919e8f6d859459ef0f6dd185d0b87d0a
Ayman <aymanuddin1111.au@gmail.com>
2024-08-07 20:23:33 +0530

    Update:- shared variables

--- src/app/shared/service/shared.service.ts
+++ src/app/shared/service/shared.service.ts
@@ -5,5 +5,36 @@ import { Injectable } from '@angular/core';
 })
 export class SharedService {
   constructor() { }
+  socialIcons = [
+    {
+      icon: '../../../assets/icons/social icons/fb-svg.svg',
+    },
+    {
+      icon: '../../../assets/icons/social icons/ig-svg.svg',
+    },
+    {
+      icon: '../../../assets/icons/social icons/linkdn-svg.svg',
+    },
+    {
+      icon: '../../../assets/icons/social icons/wa-svg.svg',
+    },
+    {
+      icon: '../../../assets/icons/social icons/X-svg.svg',
+    },
+  ];
+  jobFeatures = [
+    {
+      title: 'Customer service',
+      description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+      title: '140+',
+      description: 'Open Positions'
+    },
+    {
+      title: '19k+',
+      description: 'Active  service profiles'
+    },
+  ]
 
 }
