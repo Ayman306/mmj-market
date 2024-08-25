6d832964d1372331ef60b0462a5ce9d8cfba7452
Ayman <aymanuddin1111.au@gmail.com>
2024-08-25 17:16:13 +0530

    Update:- Search and filter UI

--- src/app/pages/all-jobs/all-jobs.component.ts
+++ src/app/pages/all-jobs/all-jobs.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/service/api.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 import { AsyncPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { SharedService } from '../../shared/service/shared.service';
 import { TimeAgoPipe } from '../../utils/time-ago.pipe';
 import { JobcardComponent } from '../../shared/components/jobcard/jobcard.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all-jobs',
   standalone: true,
-  imports: [AsyncPipe, JobcardComponent, UpperCasePipe],
+  imports: [AsyncPipe, JobcardComponent, UpperCasePipe, FormsModule],
   templateUrl: './all-jobs.component.html',
   styleUrl: './all-jobs.component.scss',
 })
 export class AllJobsComponent implements OnInit {
   constructor(private _apiService: ApiService, private route: Router, private shared: SharedService) { }
   jobPost$!: Observable<any[]>;
+  private searchSubject: Subject<string> = new Subject();
+  options = [
+    { label: 'Anytime', value: 'anytime' },
+    { label: 'Past 24 hours', value: 'new' },
+    { label: 'Past weeek', value: 'past_week' },
+    { label: 'Past month', value: 'past_month' },
+  ];
 
   ngOnInit(): void {
+    this.searchSubject.pipe(debounceTime(500)).subscribe((searchText) => {
+      this.logSearch(searchText);
+    });
+
     this.jobPost$ = this._apiService.getAllJobs().pipe(
       map((response: any) => response?.result?.map((res: any) => ({
         ...res.jobpost.contact_detail,
@@ -44,5 +56,20 @@ export class AllJobsComponent implements OnInit {
       });
 
     }
+  }
+
+  onSearch(event: any) {
+    const searchText = event.target.value;
+    this.searchSubject.next(searchText); // Emit the search text to the subject
+  }
+
+  logSearch(searchText: string) {
+    console.log('Searched text:', searchText);
+  }
+  selectedValue: string = '';
+
+  onSelect(option: any) {
+    this.selectedValue = option.value;
+    console.log('Selected value:', this.selectedValue);
   }
 }
